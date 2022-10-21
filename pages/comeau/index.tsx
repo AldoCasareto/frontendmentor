2de69489de3538fa116a82279eb8a86cd9fe591d
Aldo <acasareto@gmail.com>
2022-10-21 20:27:03 +0200

    css playground

---
+++ pages/comeau/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const index = () => {
+  return (
+    <div>
+      <div class='card'>
+        <p>
+          Otters have long, slim bodies and relatively short limbs. Their most striking anatomical
+          features are the powerful webbed feet used to swim, and their seal-like abilities holding
+          breath underwater.
+        </p>
+        <div className='stretched-content'>
+          <img alt='A cute otter in water' src='illustration-intro.svg' />
+        </div>
+        <p>
+          More importantly, otters are glorious water dogs, playful and curious. The otter, no
+          other, is the best animal.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default index;
